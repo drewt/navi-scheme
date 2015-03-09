68a70bd3bca129e98ff1fa410bc2053421cac115
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2015-03-08 17:47:04 -0700

    make install target
    Currently this only puts navii in $(bindir), so it is still necessary to
    run it from a directory containing the library hierarchy.

--- rules.mk
+++ rules.mk
@@ -39,38 +39,41 @@ distclean: clean
 	$(call cmd,dep)
 
 # CC for program object files (.o)
-quiet_cmd_cc    = CC      $@
-      cmd_cc    = $(CC) -c $(CPPFLAGS) $(ALLCFLAGS) $(1) -o $@ $<
+quiet_cmd_cc      = CC      $@
+      cmd_cc      = $(CC) -c $(CPPFLAGS) $(ALLCFLAGS) $(1) -o $@ $<
 
 # create archive
-quiet_cmd_ar    = AR      $@
-      cmd_ar    = $(AR) $(ARFLAGS) $@ $^
+quiet_cmd_ar      = AR      $@
+      cmd_ar      = $(AR) $(ARFLAGS) $@ $^
 
-quiet_cmd_ccas  = AS      $@
-      cmd_ccas  = $(CC) -c $(CPPFLAGS) $(1) -o $@ $<
+quiet_cmd_ccas    = AS      $@
+      cmd_ccas    = $(CC) -c $(CPPFLAGS) $(1) -o $@ $<
 
-quiet_cmd_as    = AS      $@
-      cmd_as    = $(AS) $(ASFLAGS) -o $@ $<
+quiet_cmd_as      = AS      $@
+      cmd_as      = $(AS) $(ASFLAGS) -o $@ $<
 
 # LD for programs; optional parameter: libraries
-quiet_cmd_ld    = LD      $@
-      cmd_ld    = $(LD) $(LDFLAGS) -o $@ $^ $(1)
+quiet_cmd_ld      = LD      $@
+      cmd_ld      = $(LD) $(LDFLAGS) -o $@ $^ $(1)
 
 # generate dependencies file
-quiet_cmd_dep   = DEP     $@
-      cmd_dep   = echo "$@ `$(CC) -MM $(CPPFLAGS) $<`" > $@
+quiet_cmd_dep     = DEP     $@
+      cmd_dep     = echo "$@ `$(CC) -MM $(CPPFLAGS) $<`" > $@
 
 # call submake
-quiet_cmd_smake = MAKE    $@
-      cmd_smake = cd $@ && $(MAKE)
+quiet_cmd_smake   = MAKE    $@
+      cmd_smake   = cd $@ && $(MAKE)
 
 # generate man page from .txt file
-quiet_cmd_ttman = MAN     $@
-      cmd_ttman = doc/ttman $< $@
+quiet_cmd_ttman   = MAN     $@
+      cmd_ttman   = doc/ttman $< $@
 
 # generate man page as plain text file
-quiet_cmd_groff = GROFF   $@
-      cmd_groff = groff -man -Tascii $< | col -bx > $@
+quiet_cmd_groff   = GROFF   $@
+      cmd_groff   = groff -man -Tascii $< | col -bx > $@
+
+quiet_cmd_install = INSTALL $(1)
+      cmd_install = $(INSTALL) $(1)
 
 # cmd macro (taken from kbuild)
 cmd = @$(if $($(quiet)cmd_$(1)),echo '  $(call $(quiet)cmd_$(1),$(2))' &&) $(call cmd_$(1),$(2))
